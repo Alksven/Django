ad421adeb69fcc8b16393d877b4e397e2b2301fb
Ven <alksven@gmail.com>
2023-10-22 17:25:38 +0300

    Django-шаблоны

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import timeit
 
 
 def shop_index(request):
-    return HttpResponse('Hello World')
+    products = [
+        ('Laptop', 1990),
+        ('Desktop', 2990),
+        ("Smartphone", 990)
+    ]
+    context = {
+        "time": timeit.default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
