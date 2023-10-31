135850db97acc5a9575a90a749050f4e8de8b175
Ven <alksven@gmail.com>
2023-10-31 09:19:08 +0300

    Подключение моделей к админке, ModelAdmin

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopapp.models import Product, Order
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'pk', 'name', 'description_short', 'price', 'discount'
+    list_display_links = "pk", "name"
+
+    def description_short(self, obj: Product) -> str:
+        if len(obj.description) < 48:
+            return obj.description
+        return obj.description[:48] + '...'
+
+
+# admin.site.register(Product, ProductAdmin)
