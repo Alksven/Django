cf9240f38ed10926feb301334e6a8d3fdfaea8de
Ven <alksven@gmail.com>
2023-11-02 10:41:40 +0300

    Отображение и редактирование связанных записей

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 from shopapp.models import Product, Order
 
 
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
     list_display = 'pk', 'name', 'description_short', 'price', 'discount'
     list_display_links = "pk", "name"
     ordering = "pk",
@@ -16,4 +23,20 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.description[:48] + '...'
 
 
-# admin.site.register(Product, ProductAdmin)
+class ProductInline(admin.TabularInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = 'delivery_address', 'promocode', 'created_at', 'user_verbose'
+
+    def get_queryset(self, request):
+        return Order.objects.select_related('user').prefetch_related('products')
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
+
