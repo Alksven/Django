8426334e6d35655e2b828920669f4e2c8ffcf4e4
Ven <alksven@gmail.com>
2023-12-02 13:29:53 +0300

    Использование DeleteView для удаления объектов

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("products/<int:pk>/", views.ProductDetailsView.as_view(), name='product_details'),
     path("products/create/", views.ProductCreateVies.as_view(), name='product-create'),
     path("products/<int:pk>/update/", views.ProductUpdateVies.as_view(), name='product-update'),
+    path("products/<int:pk>/archive/", views.ProductDeleteVies.as_view(), name='product-archive'),
     path("orders/", views.OrdersListVies.as_view(), name='orders_list'),
     path("orders/<int:pk>/", views.OrderDetailsView.as_view(), name='orders_details'),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .forms import ProductForm, GroupForm
 from .models import Product, Order
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 import timeit
 
@@ -47,8 +47,9 @@ class ProductDetailsView(DetailView):
 
 class ProductListVies(ListView):
     template_name = 'shopapp/products-list.html'
-    model = Product
+    # model = Product
     context_object_name = "products"
+    queryset = Product.objects.filter(archived=False)
 
 
 class ProductCreateVies(CreateView):
@@ -69,6 +70,15 @@ class ProductUpdateVies(UpdateView):
         )
 
 
+class ProductDeleteVies(DeleteView):
+    model = Product
+    success_url = reverse_lazy("shopapp:products_list")
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
 
 
 class OrdersListVies(ListView):
