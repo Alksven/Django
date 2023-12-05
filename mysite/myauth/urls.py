2fc204250522c15d13b0ee09c9fe0d1dccc60203
Ven <alksven@gmail.com>
2023-12-05 10:22:13 +0300

    Logout

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = 'myauth'
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="myauth/login.html", redirect_authenticated_user=True), name='login'),
+    # path("logout/", views.logout_view, name='logout'),
+    path("logout/", views.MyLogoutView.as_view(), name='logout'),
     path("cookie/get/", views.get_cookie_view, name='cookie-get'),
     path("cookie/set/", views.set_cookie_view, name='cookie-set'),
     path("session/get/", views.get_session_view, name='session-get'),
