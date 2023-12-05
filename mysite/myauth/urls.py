cd7b00772bb2cea13e0bd72c433e4f0c2e3d3fe4
Ven <alksven@gmail.com>
2023-12-05 09:34:49 +0300

    Стандартные view для аутентификации

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
+
+
 app_name = 'myauth'
 urlpatterns = [
-    path("login/", views.login_view, name='login'),
+    path("login/", LoginView.as_view(template_name="myauth/login.html", redirect_authentication_user=True), name='login'),
 
 ]
