e07042c2b6cf11fe508da39f34443f6b4802c49b
Ven <alksven@gmail.com>
2023-12-05 10:06:02 +0300

    Пользователи и сессии, куки

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 app_name = 'myauth'
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="myauth/login.html", redirect_authentication_user=True), name='login'),
-
+    path("login/", LoginView.as_view(template_name="myauth/login.html", redirect_authenticated_user=True), name='login'),
+    path("cookie/get/", views.get_cookie_view, name='cookie-get'),
+    path("cookie/set/", views.set_cookie_view, name='cookie-set'),
+    path("session/get/", views.get_session_view, name='session-get'),
+    path("session/set/", views.set_session_view, name='session-set'),
 ]
